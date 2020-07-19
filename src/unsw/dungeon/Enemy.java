42b903cce0a2a9585c63e719654c6175fecb005c
Jeremy Yip <jeremyyip98@gmail.com>
2020-07-19 23:01:09 +1000

    implementing movement of enemy

--- src/unsw/dungeon/Enemy.java
+++ src/unsw/dungeon/Enemy.java
@@ -48,12 +48,59 @@ public class Enemy extends Entity implements Moveable, PlayerObserver{
     @Override
     public void setPosition(int x, int y) {
         // TODO Auto-generated method stub
-
+        this.x().set(x);
+        this.y().set(y);
     }
 
     @Override
     public void update(Player p) {
-        // this is where we can move the enemy
-    }
 
+        // this is where we can move the enemy
+        if (p.getX() > this.getX()) {
+            if (right) {
+
+            }
+            this.moveRight();
+        } else if (p.getX() < this.getX()) {
+            this.moveLeft();
+        } else if (p.getX() == this.getX()) {
+            if (p.getY() > this.getY()) {
+                this.moveDown();
+            } else if (p.getY() < this.getY()) {
+                this.moveUp();
+            }
+        }
+    }
+    public void move(String direction) {
+        boolean up = dungeon.checkIsWalkAllowed(this, getX(), getY() - 1);
+        boolean down = dungeon.checkIsWalkAllowed(this, getX(), getY() + 1);
+        boolean left = dungeon.checkIsWalkAllowed(this, getX() - 1, getY());
+        boolean right = dungeon.checkIsWalkAllowed(this, getX() + 1, getY());
+        /*
+        while(true) {
+            if ()
+            String possibleDirection = 
+        }
+        switch (direction) {
+            case "Up":
+                if (up) {
+                    this.moveUp();
+                } else 
+                break;
+            case "Down":
+                break;
+            case "Left":
+                break;
+            case "Right":
+                break;
+        }
+        while(true){
+            if (direction.equals("right")) {
+                if (right) {
+                    this.moveRight();
+                }
+            }
+        }
+        */
+    }
 }
