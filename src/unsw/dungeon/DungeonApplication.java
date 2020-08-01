c55ab3bae718b9888886e54b92d0c676a6c6406d
Jeremy Yip <jeremyyip98@gmail.com>
2020-08-01 18:47:05 +1000

    Initial Commit

--- src/unsw/dungeon/DungeonApplication.java
+++ src/unsw/dungeon/DungeonApplication.java
@@ -8,16 +8,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DungeonApplication extends Application {
 
+    Stage window;
+    Scene scene1, scene2;
+
     @Override
     public void start(Stage primaryStage) throws IOException {
+        window = primaryStage;
         primaryStage.setTitle("Dungeon");
 
+        Label label1 = new Label("Welcome to the Dungeon Game implemented by George and Jeremy!");
+        Button button1 = new Button("Start Game");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        // First Layout
+        VBox layout1  = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 600, 600);
+
+        // Gaming Layout
+
         //JSONObject json = null;
-        DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("treasureTest.json");
+        DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("maze.json");
 
         //DungeonControllerLoader dungeonLoader = new DungeonControllerLoader(json);
 
@@ -26,13 +44,17 @@ public class DungeonApplication extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("DungeonView.fxml"));
         loader.setController(controller);
         Parent root = loader.load();
-        Scene scene = new Scene(root);
+        //Scene scene = new Scene(root);
+        scene2 = new Scene(root);
         root.requestFocus();
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //primaryStage.setScene(scene2);
+        //primaryStage.show();
+        window.setScene(scene1);
+        window.show();
     }
 
     public static void main(String[] args) {
         launch(args); 
     }
+
 }
--- src/unsw/dungeon/DungeonController.java
+++ src/unsw/dungeon/DungeonController.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * A JavaFX controller for the dungeon.
